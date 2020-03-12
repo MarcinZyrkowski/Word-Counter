182aae8952eb5832646ff06de7f8527dc20bf7a5
mzyrkowski <mzyrkowski@griddynamics.com>
2020-03-12 10:44:11 +0100

    add plugins which can generate jar file

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class ClassTest {
 
     @Test
-    public void ExistsAndNotEmpty(){
+    public void ExistsAndNotEmpty() {
 
         try {
             File file = new File("try.txt");
@@ -15,69 +15,68 @@ public class ClassTest {
             long length = file.length();
             Assert.assertTrue(exists);
             Assert.assertNotEquals(0, length);
-        }catch (Exception e){}
+        } catch (Exception e) {
+        }
 
     }
 
     @Test
-    public void FileDoesntExist(){
+    public void FileDoesntExist() {
 
         try {
             File file = new File("x.txt");
             boolean exists = file.exists();
             Assert.assertFalse(exists);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    public void WrongFlagAndGoodFile(){
+    public void WrongFlagAndGoodFile() {
 
-        try{
-            WordCounter wordCounter = new WordCounter("test.txt","incorrect flag");
+        try {
+            WordCounter wordCounter = new WordCounter("test.txt", "incorrect flag");
             int wrongFlag = wordCounter.Count();
-            Assert.assertEquals(wrongFlag,-1);
+            Assert.assertEquals(wrongFlag, -1);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    public void WrongFileAndGoodFlag(){
+    public void WrongFileAndGoodFlag() {
 
-        try{
-            WordCounter wordCounter = new WordCounter("x.txt","w");
+        try {
+            WordCounter wordCounter = new WordCounter("x.txt", "w");
             int wrongFile = wordCounter.Count();
-            Assert.assertEquals(wrongFile,-1);
-        }catch(Exception e){
+            Assert.assertEquals(wrongFile, -1);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    public void CheckTestFile(){
+    public void CheckTestFile() {
 
-        try{
-            WordCounter wordCounterLine = new WordCounter("test.txt","l");
+        try {
+            WordCounter wordCounterLine = new WordCounter("test.txt", "l");
             int numberOfLines = wordCounterLine.Count();
-            WordCounter wordCounterWords = new WordCounter("test.txt","w");
+            WordCounter wordCounterWords = new WordCounter("test.txt", "w");
             int numberOfWords = wordCounterWords.Count();
-            WordCounter wordCounterCharacters = new WordCounter("test.txt","c");
+            WordCounter wordCounterCharacters = new WordCounter("test.txt", "c");
             int numberOfCharacters = wordCounterCharacters.Count();
 
-            Assert.assertEquals(numberOfLines,3);
-            Assert.assertEquals(numberOfWords,5);
-            Assert.assertEquals(numberOfCharacters,9);
+            Assert.assertEquals(numberOfLines, 3);
+            Assert.assertEquals(numberOfWords, 5);
+            Assert.assertEquals(numberOfCharacters, 9);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
     }
-
-
 
 
 }
