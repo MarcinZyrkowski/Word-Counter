08557e4461f9a85bc44f8975fcded4108f022cd2
mzyrkowski <mzyrkowski@griddynamics.com>
2020-03-25 09:35:42 +0100

    change the path to file

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,31 @@
 package com.company;
 
+
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Counter counter = new Counter();
-        int numberOfLines = counter.countLines(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/try.txt"));
-        int numberOfWords = counter.countWords(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/try.txt"));
-        int numberOfCharacters = counter.countCharacters(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/try.txt"));
+        Scanner scannerOne = new Scanner(System.in);
+        System.out.println("please type path to file and get number of lines");
+        String pathOne = scannerOne.nextLine();
+        File file1 = new File(pathOne);
+
+        Scanner scannerTwo = new Scanner(System.in);
+        System.out.println("please type path to file and get number of words");
+        String pathTwo = scannerTwo.nextLine();
+        File file2 = new File(pathTwo);
+
+        Scanner scannerThree = new Scanner(System.in);
+        System.out.println("please type path to file and get number of characters");
+        String pathThree = scannerThree.nextLine();
+        File file3 = new File(pathThree);
+
+        WordCounter counter = new Counter();
+        int numberOfLines = counter.countLines(file1);
+        int numberOfWords = counter.countWords(file2);
+        int numberOfCharacters = counter.countCharacters(file3);
         System.out.println(numberOfLines);
         System.out.println(numberOfWords);
         System.out.println(numberOfCharacters);
--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -1,51 +1,113 @@
 import com.company.Counter;
+import com.company.WordCounter;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class ClassTest {
 
 
     @Test
+    public void lookingForPath() {
+        Path path = Paths.get("src/test/java/x.txt");
+        String string = path.toAbsolutePath().toString();
+        File file = new File(string);
+
+        try {
+            Scanner read = new Scanner(file);
+            while (read.hasNextLine()) {
+                System.out.println(read.nextLine());
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    @Test
     public void existsAndNotEmpty() {
-        Counter counter = new Counter();
-        int numberOfCharacters = counter.countCharacters(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/src/test/java/testFileWith3Lines5Words9Characters.txt"));
+
+        Path path = Paths.get("src/test/java/testFileWith3Lines5Words9Characters.txt");
+        String string = path.toAbsolutePath().toString();
+        File file = new File(string);
+
+        WordCounter counter = new Counter();
+        int numberOfCharacters = counter.countCharacters(file);
         Assert.assertNotEquals(-1, numberOfCharacters);
-        Assert.assertNotEquals(0,numberOfCharacters);
+        Assert.assertNotEquals(0, numberOfCharacters);
 
     }
 
     @Test
     public void fileDoesntExist() {
 
-            Counter counter = new Counter();
-            int numberOfCharacters = counter.countCharacters(new File("/Users/wrong_paths.txt"));
-            Assert.assertEquals(numberOfCharacters,-1);
+        Path path = Paths.get("src/test/java/wrong_file.txt");
+        String string = path.toAbsolutePath().toString();
+        File file = new File(string);
+
+        WordCounter counter = new Counter();
+        int numberOfCharacters = counter.countCharacters(file);
+        Assert.assertEquals(numberOfCharacters, -1);
 
     }
 
     @Test
     public void checkTestFile() {
 
-            Counter counter = new Counter();
-            int numberOfLines = counter.countLines(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/src/test/java/testFileWith3Lines5Words9Characters.txt"));
-            int numberOfWords = counter.countWords(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/src/test/java/testFileWith3Lines5Words9Characters.txt"));
-            int numberOfCharacters = counter.countCharacters(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/src/test/java/testFileWith3Lines5Words9Characters.txt"));
+        Path path = Paths.get("src/test/java/testFileWith3Lines5Words9Characters.txt");
+        String string = path.toAbsolutePath().toString();
+        File file = new File(string);
 
-            Assert.assertEquals(numberOfLines, 3);
-            Assert.assertEquals(numberOfWords, 5);
-            Assert.assertEquals(numberOfCharacters, 9);
+        WordCounter counter = new Counter();
+        int numberOfLines = counter.countLines(file);
+        int numberOfWords = counter.countWords(file);
+        int numberOfCharacters = counter.countCharacters(file);
+
+        Assert.assertEquals(numberOfLines, 3);
+        Assert.assertEquals(numberOfWords, 5);
+        Assert.assertEquals(numberOfCharacters, 9);
 
     }
 
     @Test
-    public void existingEmptyFile(){
-        Counter counter = new Counter();
-        int numberOfCharacters = counter.countCharacters(new File("/Users/mzyrkowski/IdeaProjects/Ważne - zadania/007_Word_Counter_New/src/test/java/emptyFile.txt"));
-        Assert.assertEquals(0,numberOfCharacters);
-        Assert.assertNotEquals(-1,numberOfCharacters);
+    public void existingEmptyFile() {
 
+        Path path = Paths.get("src/test/java/emptyFile.txt");
+        String string = path.toAbsolutePath().toString();
+        File file = new File(string);
+
+        WordCounter counter = new Counter();
+        int numberOfCharacters = counter.countCharacters(file);
+        Assert.assertEquals(0, numberOfCharacters);
+
+    }
+
+    @Test
+    public void testCharactersMethod() {
+        String string = "12345 12345 12345";
+        Scanner scanner = new Scanner(string);
+        int characters = 0;
+        while (scanner.hasNext()) {
+            String a = scanner.next();
+            characters += a.length();
+        }
+        Assert.assertEquals(characters, string.replaceAll("\\s", "").toCharArray().length);
+    }
+
+    @Test
+    public void testWordsMethod() {
+        String string = "12345 12345 12345";
+        Scanner scanner = new Scanner(string);
+        int words = 0;
+        while (scanner.hasNext()) {
+            scanner.next();
+            words++;
+        }
+
+        Assert.assertEquals(words, string.split("\\s").length);
     }
 
 
