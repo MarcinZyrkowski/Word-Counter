d17d01ec029fe881b1e5820567a35e0355e23b56
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-11-29 13:27:24 +0100

    added running app method for tests

--- src/test/java/com/company/tests/SpringBaseTestNGTest.java
+++ src/test/java/com/company/tests/SpringBaseTestNGTest.java
@@ -1,8 +1,10 @@
 package com.company.tests;
 
+import com.company.WordCounterApp;
 import com.company.conf.SpringConfiguration;
 import com.company.serviceinterface.CounterServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeSuite;
@@ -17,5 +19,12 @@ public class SpringBaseTestNGTest extends AbstractTestNGSpringContextTests {
     @Override
     protected void springTestContextPrepareTestInstance() throws Exception {
         super.springTestContextPrepareTestInstance();
+    }
+
+    // runs app
+    @BeforeSuite
+    public void run() {
+        SpringApplication wordCounterApp = new SpringApplication(WordCounterApp.class);
+        wordCounterApp.run();
     }
 }
