271e488814b79f372c1dc0a94ce63a4ae4fc4f52
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2023-08-28 18:25:15 +0200

    added testng annotations

--- src/test/java/com/company/tests/counter/WordCounterTest.java
+++ src/test/java/com/company/tests/counter/WordCounterTest.java
@@ -7,7 +7,6 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.DisplayName;
 import org.testng.annotations.Test;
 
 import java.util.StringJoiner;
@@ -16,13 +15,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Feature("Word counter: counting feature and get feature")
 @Story("Word counter test")
-@DisplayName("REST: Word counter test")
 public class WordCounterTest extends SpringBaseTestNGTest {
     public static final String HELLO_WORLD_MESSAGE = "hello world";
 
-    @Test
+    @Test(description = "Verify counting feature")
     @Description("Verify counting feature")
-    @DisplayName("Verify counting feature")
     public void verifyCountingFeature() {
         StringJoiner stringJoiner = new StringJoiner("\n");
         stringJoiner.add(RandomStringUtils.random(10, true, false));
@@ -44,9 +41,8 @@ public class WordCounterTest extends SpringBaseTestNGTest {
                 .isEqualTo(10 + 5);
     }
 
-    @Test
+    @Test(description = "Verify get hello world")
     @Description("Verify get hello world")
-    @DisplayName("Verify get hello world")
     public void getHelloWorld() {
         String message = counterServiceInterface.getMessage();
         assertThat(message)
